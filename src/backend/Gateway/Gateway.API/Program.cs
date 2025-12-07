a9b68dc9eb8931c84d82170ca28fb328774aa5b6
SinmisolaE <sinmisolaajao@gmail.com>
2025-12-07 19:35:50 +0000

    feat: Rate limiting added

--- src/backend/Gateway/Gateway.API/Program.cs
+++ src/backend/Gateway/Gateway.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add yarp as the gateway to services
@@ -19,6 +21,30 @@ builder.Services.AddCors(options =>
     });
 });
 
+// enable rate limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+    options.AddFixedWindowLimiter(policyName: "fixed", opt =>
+    {
+        opt.PermitLimit = 100;
+        opt.Window = TimeSpan.FromMinutes(1);
+    });
+
+    options.AddFixedWindowLimiter(policyName: "fixed-issuer", op =>
+    {
+        op.PermitLimit = 50;
+        op.Window = TimeSpan.FromMinutes(1);
+    });
+
+    options.OnRejected = async (context, cancellationToken) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        context.HttpContext.Response.Headers["Retry-After"] = TimeSpan.FromMinutes(1).TotalSeconds.ToString();
+        await context.HttpContext.Response.WriteAsync("Too many requests. Please try again later.", cancellationToken);
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +59,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
